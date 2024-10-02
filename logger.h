817bf456dbd932cb6700458cfb05e234dc4b6b4b
Landmand97 <64842258+Landmand97@users.noreply.github.com>
2024-10-02 11:20:45 +0200

    initial commit

---
+++ logger.h
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+
+#ifndef logger_h
+#define logger_h
+
+enum log_levels
+{
+    INFO,
+    DEBUG,
+    WARNING,
+    ERROR
+};
+#endif
+
+void log(log_levels level, const char *message, const char t);
+
+void set_log_level(log_levels level);
+
+String get_log_level();
